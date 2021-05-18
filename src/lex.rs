98a4e3328d553c51319b343bc1f83c502852f6dc
Ryan Zambrotta <rtzamis2@gmail.com>
2021-05-18 09:11:25 -0400

    Reserve keywords and parse trailing if clause
    The Lexer will now flag a reserved keyword. These are
    words that are common keywords in other languages
    that conflict or duplicate the meaning of keywords in this
    language.
    
    The Parse now parses Python-like trailing if clause
    Example:
        1 + 2 if condition else 5
    
        the two possible return values are 3 and 5 based on "condition"
        and _not_ 3 and 6. In other words, this is desugared into
    
        if condition
            1 + 2
        else
            5
        end

--- src/lex.rs
+++ src/lex.rs
@@ -1,5 +1,9 @@
 
-use crate::ast::token::{TokenData, Token, KwKind};
+use std::{collections::HashMap};
+
+use lazy_static::lazy_static;
+
+use crate::ast::token::{TokenData, Token, KwKind, ReservedKind};
 
 mod scan;
 
@@ -14,24 +18,9 @@ fn process_token_kw<'s>(tok: TokenData<'s>) -> TokenData<'s>{
     match tok.kind{
         Token::Ident => {
             // TODO: lazy_static HashMap
-            let new_kind = match tok.span{
-                "fun"   => Token::Kw(KwKind::Fun),   
-                "extern"=> Token::Kw(KwKind::Extern),
-                "end"   => Token::Kw(KwKind::End),
-                "do"    => Token::Kw(KwKind::Do),
-                "mod"   => Token::Kw(KwKind::Mod),
-                "if"    => Token::Kw(KwKind::If),
-                "else"  => Token::Kw(KwKind::Else),
-                "val"   => Token::Kw(KwKind::Val),
-                "let"   => Token::Kw(KwKind::Let),
-                "in"    => Token::Kw(KwKind::In),
-                "mut"   => Token::Kw(KwKind::Mut),
-                // "loop"  => Token::Kw(KwKind::Loop),
-                "while" => Token::Kw(KwKind::While),
-                // "from"  => Token::Kw(KwKind::From),
-                // "import"=> Token::Kw(KwKind::Export),
-                // "export"=> Token::Kw(KwKind::Export),
-                _        => tok.kind,
+            let new_kind = match _TOKEN_KW_TABLE.get(tok.span){
+                Some(kind) => kind.clone(),
+                _ => tok.kind,
             };
 
             TokenData::new(new_kind, tok.span, tok.loc)
@@ -39,4 +28,86 @@ fn process_token_kw<'s>(tok: TokenData<'s>) -> TokenData<'s>{
 
         _ => tok,
     }
+}
+
+lazy_static!{
+    static ref _TOKEN_KW_LIST: [(&'static str, Token); 76] = [        
+        ("fun"      , Token::Kw(KwKind::Fun)),
+        ("extern"   , Token::Kw(KwKind::Extern)),
+        ("end"      , Token::Kw(KwKind::End)),
+        ("do"       , Token::Kw(KwKind::Do)),
+        ("mod"      , Token::Kw(KwKind::Mod)),
+        ("if"       , Token::Kw(KwKind::If)),
+        ("else"     , Token::Kw(KwKind::Else)),
+        ("val"      , Token::Kw(KwKind::Val)),
+        ("let"      , Token::Kw(KwKind::Let)),
+        ("in"       , Token::Kw(KwKind::In)),
+        ("mut"      , Token::Kw(KwKind::Mut)),
+        ("while"    , Token::Kw(KwKind::While)),
+        ("loop"     , Token::Reserved(ReservedKind::Loop)),
+        ("from"     , Token::Reserved(ReservedKind::FromKw)),
+        ("import"   , Token::Reserved(ReservedKind::Import)),
+        ("export"   , Token::Reserved(ReservedKind::Export)),
+        ("def"      , Token::Reserved(ReservedKind::Def)),
+        ("fn"       , Token::Reserved(ReservedKind::Fn)),
+        ("this"     , Token::Reserved(ReservedKind::This)),
+        ("self"     , Token::Reserved(ReservedKind::KwSelf)),
+        ("enum"     , Token::Reserved(ReservedKind::Enum)),
+        ("struct"   , Token::Reserved(ReservedKind::Struct)),
+        ("class"    , Token::Reserved(ReservedKind::Class)),
+        ("rec"      , Token::Reserved(ReservedKind::Rec)),
+        ("data"     , Token::Reserved(ReservedKind::Data)),
+        ("type"     , Token::Reserved(ReservedKind::Type)),
+        ("alias"    , Token::Reserved(ReservedKind::Alias)),
+        ("use"      , Token::Reserved(ReservedKind::Use)),
+        ("using"    , Token::Reserved(ReservedKind::Using)),
+        ("as"       , Token::Reserved(ReservedKind::As)),
+        ("pub"      , Token::Reserved(ReservedKind::Pub)),
+        ("exposing" , Token::Reserved(ReservedKind::Exposing)),
+        ("async"    , Token::Reserved(ReservedKind::Async)),
+        ("await"    , Token::Reserved(ReservedKind::Await)),
+        ("for"      , Token::Reserved(ReservedKind::For)),
+        ("in"       , Token::Reserved(ReservedKind::In)),
+        ("match"    , Token::Reserved(ReservedKind::Match)),
+        ("case"     , Token::Reserved(ReservedKind::Case)),
+        ("switch"   , Token::Reserved(ReservedKind::Switch)),
+        ("and"      , Token::Reserved(ReservedKind::And)),
+        ("or"       , Token::Reserved(ReservedKind::Or)),
+        ("xor"      , Token::Reserved(ReservedKind::Xor)),
+        ("ref"      , Token::Reserved(ReservedKind::Ref)),
+        ("var"      , Token::Reserved(ReservedKind::Var)),
+        ("const"    , Token::Reserved(ReservedKind::Const)),
+        ("global"   , Token::Reserved(ReservedKind::Global)),
+        ("local"    , Token::Reserved(ReservedKind::Local)),
+        ("new"      , Token::Reserved(ReservedKind::New)),
+        ("del"      , Token::Reserved(ReservedKind::Del)),
+        ("delete"   , Token::Reserved(ReservedKind::Delete)),
+        ("assert"   , Token::Reserved(ReservedKind::Assert)),
+        ("defer"    , Token::Reserved(ReservedKind::Defer)),
+        ("move"     , Token::Reserved(ReservedKind::Move)),
+        ("go"       , Token::Reserved(ReservedKind::Go)),
+        ("try"      , Token::Reserved(ReservedKind::Try)),
+        ("catch"    , Token::Reserved(ReservedKind::Catch)),
+        ("break"    , Token::Reserved(ReservedKind::Break)),
+        ("continue" , Token::Reserved(ReservedKind::Continue)),
+        ("goto"     , Token::Reserved(ReservedKind::GoTo)),
+        ("impl"     , Token::Reserved(ReservedKind::Impl)),
+        ("return"   , Token::Reserved(ReservedKind::Return)),
+        ("yield"    , Token::Reserved(ReservedKind::Yield)),
+        ("throw"    , Token::Reserved(ReservedKind::Throw)),
+        ("raise"    , Token::Reserved(ReservedKind::Raise)),
+        ("static"   , Token::Reserved(ReservedKind::Static)),
+        ("trait"    , Token::Reserved(ReservedKind::Trait)),
+        ("super"    , Token::Reserved(ReservedKind::Super)),
+        ("unsafe"   , Token::Reserved(ReservedKind::Unsafe)),
+        ("where"    , Token::Reserved(ReservedKind::Where)),
+        ("final"    , Token::Reserved(ReservedKind::Final)),
+        ("virtual"  , Token::Reserved(ReservedKind::Virtual)),
+        ("override" , Token::Reserved(ReservedKind::Override)),
+        ("except"   , Token::Reserved(ReservedKind::Except)),
+        ("dyn"      , Token::Reserved(ReservedKind::Dyn)),
+        ("bit"      , Token::Reserved(ReservedKind::Bit)),
+        ("flag"      , Token::Reserved(ReservedKind::Flag)),
+    ];
+    static ref _TOKEN_KW_TABLE: HashMap<&'static str, Token> = _TOKEN_KW_LIST.iter().cloned().collect();
 }
