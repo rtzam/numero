be92e301d38f25954bef7637247ecb266a423ef7
Ryan Zambrotta <rtzamis2@gmail.com>
2021-09-10 10:10:44 -0400

    Add cli help for subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -54,13 +54,6 @@ impl<'a, 'b> BuildFileCli<'a, 'b> for App<'a, 'b> {
     }
 }
 
-fn make_shell_completion_subcommand<'a,'b>() -> App<'a,'b>{
-    SubCommand::with_name("shell")
-        .arg(
-            Arg::with_name("shell")
-            .required(true)
-            .possible_values(&Shell::variants()))
-}
 
 pub fn make_cli<'a, 'b>() -> App<'a, 'b> {
     App::new("The Nosh Compiler")
@@ -72,25 +65,34 @@ pub fn make_cli<'a, 'b>() -> App<'a, 'b> {
                 .takes_value(true)
                 .value_name("level")
                 .default_value("0")
-                .help("display increasing levels of output"),
-        )
+                .help("display increasing levels of output"))
         .subcommand(
             SubCommand::with_name("build")
                 .alias("b")
+                .about("Compile program to object file")
                 .arg(
                     Arg::with_name("emit")
                         .long("emit")
                         .takes_value(true)
                         .possible_values(&["ast", "asm", "llvm"]),
                 )
-                .append_build_file_args(),
-        )
-        .subcommand(SubCommand::with_name("repl"))
+                .append_build_file_args())
+        .subcommand(
+            SubCommand::with_name("repl")
+                    .about("Run Interactive REPL"))
         .subcommand(
             SubCommand::with_name("run")
                 .alias("r")
-                .append_build_file_args(),
-        )
-        .subcommand(SubCommand::with_name("check"))
-        .subcommand(make_shell_completion_subcommand())
+                .about("Execute program using LLVM JIT")
+                .append_build_file_args())
+        .subcommand(
+            SubCommand::with_name("check")
+                .about("Check program correctness without generating object file"))
+        .subcommand(
+            SubCommand::with_name("shell")
+                .about("Generate shell completion instructions for nosh")
+                .arg(
+                    Arg::with_name("shell")
+                    .required(true)
+                    .possible_values(&Shell::variants())))
 }
